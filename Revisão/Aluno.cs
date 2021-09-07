d8b923501b6b60ae809d0d6be84cfc83e7422031
gustavo <guguines@hotmail.com>
2021-09-07 20:03:53 -0300

    Struct Aluno inserida, inserção de aluno implementada

---
+++ Revisão/Aluno.cs
@@ -0,0 +1,8 @@
+namespace Revisão
+{
+    public struct Aluno // deveria ser uma classe, orém o exemplo é muito simples
+    {
+        public string Name { get; set; }
+        public decimal Nota { get; set; }
+    }
+}
--- Revisão/Revisao.cs
+++ Revisão/Revisao.cs
@@ -6,24 +6,40 @@ namespace Revisão
     {
         static void Main(string[] args)
         {
-            char opcaoUsuario;
+            string opcaoUsuario;
+            Aluno[] alunos = new Aluno[5]; //cinco alunos no array
+            var indexStudent = 0;
             do
             {
                 menu();
-                opcaoUsuario = Console.Read();
-                if(opcaoUsuario == '1')
+                opcaoUsuario = Console.ReadLine();
+                if(opcaoUsuario == "1")
                 {
-                    //todo: adicionar alunos
+                    Console.WriteLine("Informe o nome do aluno:");
+                    Aluno newAluno = new Aluno();
+                    var nameNewStudent = Console.ReadLine();
+                    newAluno.Name = nameNewStudent;
+                    Console.WriteLine("Informe a nota desse aluno:");
+                    if(decimal.TryParse(Console.ReadLine(), out decimal nota)) // TryParse pergunta se pode fazer o parse do string para decimal, se puder, retorna true e o out decimal nota terá seu valor
+                    {
+                        newAluno.Nota = nota;
+                    }
+                    else // caso não de para fazer o parser, o usuário informou um valor que não é decimal
+                    {
+                        throw new ArgumentException("Valor da nota deve ser um decimal");
+                    }
+                    alunos[indexStudent] = newAluno;
+                    indexStudent = indexStudent + 1;
                 } 
-                else if(opcaoUsuario == '2')
+                else if(opcaoUsuario == "2")
                 {
                     //todo: listas todos os alunos
                 } 
-                else if (opcaoUsuario == '3')
+                else if (opcaoUsuario == "3")
                 {
                     //todo: calcular édia geral
                 } 
-                else if(opcaoUsuario == '4')
+                else if(opcaoUsuario == "4")
                 {
                     Console.WriteLine("Programa finalizado.");
                 } 
@@ -31,15 +47,15 @@ namespace Revisão
                 {
                     Console.WriteLine("Input inválido.");
                 }
-            } while(opcaoUsuario != '4');  
+            } while(opcaoUsuario != "4");  
         }
-        static void menu()
+        private static void menu()
         {
-            Console.WriteLine("Informe uma opção:");
+            Console.WriteLine("\nInforme uma opção:");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar todos os alunos");
             Console.WriteLine("3 - Calcular média geral");
-            Console.WriteLine("4 - Finalizar programa")
+            Console.WriteLine("4 - Finalizar programa\n");
         }
     }
 }
