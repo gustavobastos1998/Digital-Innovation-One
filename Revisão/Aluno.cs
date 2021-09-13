80e5aa5973f89417e350435e177c6a13cf79f4cc
gustavo <guguines@hotmail.com>
2021-09-13 16:40:46 -0300

    Utilização dos conceitos de OO e implementação dos get e set

--- Revisão/Aluno.cs
+++ Revisão/Aluno.cs
@@ -2,7 +2,17 @@ namespace Revisão
 {
     public struct Aluno // deveria ser uma classe, orém o exemplo é muito simples
     {
-        public string Name { get; set; }
-        public decimal Nota { get; set; }
+        private string name;
+        private decimal nota;
+        public string Name 
+        { 
+            get => name; 
+            set => name = value; 
+        }
+        public decimal Nota 
+        { 
+            get => nota; 
+            set => nota = value;
+        }
     }
 }
